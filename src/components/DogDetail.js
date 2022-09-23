4a6c2df4035a63f298c1ad4eef593b2717921f30
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-23 14:07:34 -0700

    Add dog details

--- src/components/DogDetail.js
+++ src/components/DogDetail.js
@@ -14,9 +14,39 @@ function DogDetail(props){
       <p>Weight: {dog.dogWeight}</p>
       <p>Age: {dog.dogAge}</p>
       <hr/>
-      <h2>Likes</h2>
-      <hr/>
-      
+      <div className='likes'>
+        <h2>Likes</h2>
+        <hr/>
+        {dog.dogLikes.map((likes, index) => (
+          <tr key={index}>{likes}</tr>
+        ))}
+      </div>
+      <div className='dislikes'>
+        <h2>Dislikes</h2>
+        <hr/>
+        {dog.dogDislikes.map((dislikes, index) => (
+          <tr key={index}>{dislikes}</tr>
+        ))}
+      </div>
+      <div className='parks'>
+        <h2>Parks</h2>
+        <hr />
+        {dog.dogParks.map((parks, index) => (
+          <tr key={index}>{parks}</tr> //Eventually this key will be an id and link to a specific park. Perhaps add parks button as well. 
+        ))}
+      </div>
+      <div className='buttonControl'>
+        <button onClick={onClickingEdit}>Edit Dog</button>
+        <button onClick={()=> onClickingDelete(dog.id)}>Remove Dog</button>
+      </div>
     </React.Fragment>
   )
 }
+
+DogDetail.propTypes = {
+  dog: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+}
+
+export default DogDetail;
