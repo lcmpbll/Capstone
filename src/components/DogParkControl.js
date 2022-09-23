377a85b55ad0156240806dbc4bad032cabc0fb91
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-23 11:16:58 -0700

    Add newDogFomr

--- src/components/DogParkControl.js
+++ src/components/DogParkControl.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DogList from './DogList';
+import NewDogForm from './NewDogForm';
 
 
 function DogParkControl(){
+  const [mainDogList , setMainDogList ] = useState([]);
+  
+  const handleAddingNewDogToList = (newDog) => {
+    const newMainDogList = mainDogList.concat(newDog);
+    setMainDogList(newMainDogList);
+  }
   return( 
     <React.Fragment>
-      <DogList/>
+      <DogList dogList={mainDogList}/>
+      <NewDogForm onNewDogCreation={handleAddingNewDogToList}/>
     </React.Fragment>
     )
 }
