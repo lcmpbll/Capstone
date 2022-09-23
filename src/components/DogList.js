7a80e425ec52b7a1bf53922d7d1a244b57613aca
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-23 11:16:10 -0700

    Add dog ids

--- src/components/DogList.js
+++ src/components/DogList.js
@@ -7,7 +7,8 @@ const mainDogList = [
     dogWeight: 35,
     dogDislikes: ["childern"],
     dogLikes: ["Big dogs"],
-    dogParks: ["Alberta"]
+    dogParks: ["Alberta"],
+    id: 'jncouwirh8'
   },
   {
     dogName: 'Angus',
@@ -15,7 +16,8 @@ const mainDogList = [
     dogWeight: 150,
     dogDislikes: ["Small Dogs", " Rude People"],
     dogLikes: ["Sticks"],
-    dogParks: ["Fremont"]
+    dogParks: ["Fremont"],
+    id: 'okjncoiw34'
   },
   {
     dogName: 'Sweetie',
@@ -23,19 +25,23 @@ const mainDogList = [
     dogWeight: 12,
     dogDislikes: ["Big Dogs"],
     dogLikes: ["Loud Noises"],
-    dogParks: ["Alberta"]
+    dogParks: ["Alberta"],
+    id: 'jdfhuhe222'
   }
 ]
 function DogList(){
   return (
     <React.Fragment>
-      {mainDogList.map((dog, index) =>
-      <Dog dogName={dog.dogName}
-      dogSex={dog.dogSex}
-      dogWeight={dog.dogWeight}
-      dogDislikes={dog.dogDislikes}
-      dogLikes={dog.dogLikes}
-      dogParks={dog.dogParks}/>
+      {mainDogList.map((dog) =>
+      <div className='card' key={dog.id}>
+        <Dog dogName={dog.dogName}
+        dogSex={dog.dogSex}
+        dogWeight={dog.dogWeight}
+        dogDislikes={dog.dogDislikes}
+        dogLikes={dog.dogLikes}
+        dogParks={dog.dogParks}
+        key={dog.id}/>
+      </div>
       )}
     </React.Fragment>
   );
