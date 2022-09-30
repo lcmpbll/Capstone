825c9d605c1872bce4d331f16c0dd5be79f65a15
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-30 08:30:52 -0700

    Add propTypes

--- src/components/FriendingDog.js
+++ src/components/FriendingDog.js
@@ -2,12 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function FriendDogForm(props) {
-  const {dog} = props;
+  const {dog, mainDogList} = props;
   
-  function handleEditDogFormSubmission(event){
+  function handleFriendDogFormSubmission(event){
     
     
   }
+  
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  );
+}
+
+FriendDogForm.PropTypes = {
+  mainDogList: PropTypes.array,
+  dog: PropTypes.object,
+  
+  
 }
 
 export default FriendDogForm
