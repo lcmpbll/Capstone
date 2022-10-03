e08bb6abec955cd55c19d40f529d6f82d5705516
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-02 21:08:46 -0700

    Add user authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Header from './Header';
 import DogParkControl from './DogParkControl';
+import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator, Button} from '@aws-amplify/ui-react';
 
-function App() {
+function App({signOut}) {
   return (
    <React.Fragment>
     <Header />
     <DogParkControl/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <Button onClick={signOut}>Sign Out</Button>
     </React.Fragment>
     
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+//return to see if app can be wrapped and only accessed through sign in.
