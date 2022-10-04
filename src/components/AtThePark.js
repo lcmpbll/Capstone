cb2ffcf19d472a359f5a8e9dcedfa2120200151e
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-04 13:42:03 -0700

    Update styling

--- src/components/AtThePark.js
+++ src/components/AtThePark.js
@@ -3,19 +3,35 @@ import React from 'react';
 
 
 function AtThePark(props){
-  console.log(props);
+  //Styles
+  const mainAtTheParkStyles = {
+    display: 'flex',
+    marginRight: '20px',
+    flexDirection: 'column',
+  }
+  const dogAtTheParkStyles = {
+    marginTop: '2px',
+    margin: 'auto',
+  }
+  const parkHeaderStyle = {
+    margin: 'auto',
+  }
   
   return(
     <React.Fragment>
-      <h1>Dogs at Alberta Park</h1>
-      {props.atTheParkList.map((dog) =>
-      <div key={dog.id}>
-       <h1>{dog.dogName}, is at the park</h1>
-       {/* <button onClick = {() => props.whenDogFriendClicked(dog.id)}>Add as a friend</button>
-       May add this back in, but for now going to use edit dog to add to dogFriends */}
-       <br/>
-       </div>
-      )}
+      <div style={mainAtTheParkStyles}>
+        <div style={parkHeaderStyle}>
+          <h1>Alberta Park</h1>
+        </div>
+        {props.atTheParkList.map((dog) =>
+        <div style={dogAtTheParkStyles} key={dog.id}>
+        <h1>{dog.dogName}</h1>
+        {/* <button onClick = {() => props.whenDogFriendClicked(dog.id)}>Add as a friend</button>
+        May add this back in, but for now going to use edit dog to add to dogFriends */}
+        <br/>
+        </div>
+        )}
+      </div>
     </React.Fragment>
   );
 }
