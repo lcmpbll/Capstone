723dc3637404092a93620deb3b8595cc88396751
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-04 15:39:57 -0700

    Set up friends array

--- src/components/FriendingDog.js
+++ src/components/FriendingDog.js
@@ -3,23 +3,17 @@ import PropTypes from 'prop-types';
 
 function FriendDogForm(props) {
   const {dog, dogList} = props;
-  const [dogFriendsArray, setDogFriendsArray] = useState({
-      dogFriends: [], dogFriendsResponse: [],
-    });
-    
+  const [friendsArray, setFriendsArray] = useState([]);
+  
   const handleFriendChange = (event) => {
     const {value} = event.target;
-    const {...dogFriends} = dogFriendsArray;
-    console.log(`${dog.dogName} is friends with ${value}`);
-    setDogFriendsArray({
-      dogFriends: [...dogFriendsArray, value],
-      dogFriendsResponse: [...dogFriendsArray, value],
-    }); 
+    setFriendsArray(friendsArray => friendsArray.concat(value));
+    return friendsArray;
   }
   
   function handleFriendDogFormSubmission(event){
     event.preventDefault();
-    props.onFriendSelection({
+    props.onFriendsSelection({
       dogName: dog.dogName,
       dogWeight: dog.dogWeight,
       dogSize: dog.dogSize,
@@ -40,10 +34,10 @@ function FriendDogForm(props) {
   return (
     <React.Fragment>
       <form onSubmit={handleFriendDogFormSubmission}>
-        <h1>`Find Friends for ${dog.dogName}:`</h1>
-        <select onChange={handleFriendChange}>
+        <h1>Find Friends for {dog.dogName}:</h1>
+        <select onChange={handleFriendChange} >
           <option value='none'>--Select a friend for your dog--</option>
-          {Object.entries(dogList).filter(dogs => dogs.id !== props.dog.id).map((dogs) => <option key={dogs.id} value={dogs.id}>{dogs.dogName}</option>)}
+          {(dogList).filter(dogs => dogs.id !== props.dog.id).map((dogs) => <option key={dogs.id} value={dogs.id}>{dogs.dogName}</option>)}
         </select>
         <br/>
         <button type="submit" className='btn btn-default'>Submit</button>
