1eea9a3dfca1fc84fd2d24b517b69c40d2009c85
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-04 09:51:19 -0700

    Update main styles

--- src/components/DogParkControl.js
+++ src/components/DogParkControl.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DogList from './DogList';
 import NewDogForm from './NewDogForm';
 import DogDetail from './DogDetail';
 import AtThePark from './AtThePark';
 import FriendingDog from './FriendingDog';
+import { API } from 'aws-amplify';
+import { listDogs } from '../graphql/queries';
+import { createDog as createDogMutation, deleteDog as deleteDogMutation } from '../graphql/mutations';
+import { flushSync } from 'react-dom';
 
 
 
@@ -12,8 +16,37 @@ function DogParkControl(){
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [selectedDog, setSelectedDog] = useState(null);
   const [mainAtTheParkList, setMainAtTheParkList] = useState([]);
-  // const [atThePark, setAtThePark] = useState(false);
   const [friendingDog, setFriendingDog] = useState(false);
+  
+  // useEffect(() => {
+  //   fetchDogs();
+  // }, []);
+  
+  // async function fetchDogs() {
+  //   const apiData =  await API.graphql({ query: listDogs });
+  //   console.log(apiData);
+  //   const apiDogs = apiData.data.listDogs.items;
+  //   console.log(apiDogs);
+  //   setMainDogList(apiDogs);
+    
+  // }
+  
+  // const handleAddingNewDogToList = async (newDog) => {
+  //   await API.graphql({ query : createDogMutation, variables : { input: newDog}, });
+  //   fetchDogs();
+  //   setFormVisibleOnPage(false);
+  // }
+  
+  const handleAddingNewDogToList = (newDog) => {
+    const newMainDogList = mainDogList.concat(newDog);
+    setMainDogList(newMainDogList);
+    console.log(newMainDogList);
+    setFormVisibleOnPage(false);
+  }
+  
+  
+  
+  
   
   const handleClick = () => {
     if(selectedDog != null){
@@ -24,9 +57,13 @@ function DogParkControl(){
       setFormVisibleOnPage(!formVisibleOnPage);
     }
   }
-  const handleDeletingDog = (id) => {
-    const newMainDogList = mainDogList.filter(dog => dog.id !== id);
+  const handleDeletingDog = async ({id}) => {
+    const newMainDogList = mainDogList.filter((dog) => dog.id !== id);
     setMainDogList(newMainDogList);
+    await API.graphql({
+      query: deleteDogMutation,
+      variables: { input: {id}},
+    });
     setSelectedDog(null);
   }
   const handleChangingSelectedDog = (id) => {
@@ -53,19 +90,37 @@ function DogParkControl(){
     setMainDogList(editedDogList);
     setFriendingDog(false);
   }
-
-  
-  
-  const handleAddingNewDogToList = (newDog) => {
-    const newMainDogList = mainDogList.concat(newDog);
-    setMainDogList(newMainDogList);
-    console.log(newMainDogList);
-    setFormVisibleOnPage(false);
-  }
   
   const handleFriendingClick = () => {
     setFriendingDog(true);
   }
+  
+    // styles
+    const dogParkControlStyle = {
+      display: 'flex',
+    }
+    
+    const atTheParkStyle = {
+      position: 'absolute',
+      right: '30px',
+      
+    }
+    
+    const buttonStyle = {
+      position: 'absolute',
+      bottom: '30px',
+      left: '20px'
+    }
+    
+    const mainContentStyle = {
+      justifyContent: 'center',
+      marginLeft: '100px',
+    }
+    
+    
+  
+  //display ifs and elses
+  
   let parkList = null;
   let currentlyVisibleState = null;
   let buttonText = null;
@@ -76,18 +131,25 @@ function DogParkControl(){
     currentlyVisibleState = <DogDetail dog={selectedDog} onClickingFriend={handleFriendingClick} onClickingDelete={handleDeletingDog} onClickingGo={handleGoingToThePark}/>
     buttonText= 'Return to dog list';
   } else if(formVisibleOnPage) {
-    currentlyVisibleState = <NewDogForm onNewDogCreation={handleAddingNewDogToList} />
+    currentlyVisibleState = <NewDogForm onNewDogCreation={handleAddingNewDogToList} dogList={mainDogList} />
     buttonText = 'Return to dog list';
   } else {
     currentlyVisibleState = <DogList onDogSelection={handleChangingSelectedDog} dogList={mainDogList} />
       parkList = <AtThePark atTheParkList={mainAtTheParkList} />
     buttonText = 'Add Dog';
   }
+  
   return(
     <React.Fragment>
-      {currentlyVisibleState}
-      {parkList}
-      <button onClick={handleClick}>{buttonText}</button>
+      <div style={dogParkControlStyle} >
+        <div style={mainContentStyle}>
+          {currentlyVisibleState}
+        </div>
+        <div style={atTheParkStyle}>
+          {parkList}
+        </div>
+        <button style={buttonStyle} onClick={handleClick}>{buttonText}</button>
+      </div>
     </React.Fragment>
   );
 }
