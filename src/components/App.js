360a5eb8a599aaeaa35197d74963b3f8d18e114d
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-04 09:01:57 -0700

    Add background image

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,28 @@ import Header from './Header';
 import DogParkControl from './DogParkControl';
 import '@aws-amplify/ui-react/styles.css';
 import { withAuthenticator, Button} from '@aws-amplify/ui-react';
+import background from '../Img/background.jpg';
 
 function App({signOut}) {
+  const landingPageStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    height: '100vh'
+  }
   return (
    <React.Fragment>
     <Header />
-    <DogParkControl/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <Button onClick={signOut}>Sign Out</Button>
+    <div style={landingPageStyle}>
+      <DogParkControl/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Button onClick={signOut}>Sign Out</Button>
+    </div>
     </React.Fragment>
     
   );
