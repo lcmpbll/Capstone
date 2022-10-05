2c06f57a0125d3623b340ddb5810f1f6c1223252
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-05 16:29:31 -0700

    WIP calculating average dog age.

--- src/components/Park.js
+++ src/components/Park.js
@@ -5,7 +5,34 @@ function Park(props){
   const { atTheParkList } = props;
   let numberOfDogsAtThePark = atTheParkList.length;
   
-
+  const calculateAverageAge = (atTheParkList) => {
+    let puppies = 0; let youngAdults = 0;
+    let adults = 0; let oldDogs = 0;
+    for(let i = 0; i < atTheParkList.length; i ++){
+      if(atTheParkList[i].dogAge === 'Puppy' || atTheParkList[i].dogAge === 'Prevaccinated puppy'){
+        puppies ++;
+        console.log(puppies);
+      } else if (atTheParkList[i].dogAge === 'Juvenile' || atTheParkList[i].dogAge === "Young Adult") {
+        youngAdults ++;
+        console.log(youngAdults);
+      } else if ( atTheParkList[i].dogAge === 'Adult'){
+        adults ++;
+      } else if(atTheParkList[i].dogAge === 'Senior' || atTheParkList[i].dogAge === "Geriatric") {
+        oldDogs ++;
+      }
+    }
+    if(puppies > youngAdults && puppies > adults && puppies > oldDogs){
+      return 'Mostly Puppies';
+    } else if (youngAdults > adults && youngAdults > oldDogs){
+      return 'Mostly younger dogs';
+    } else if (adults > oldDogs){
+      return 'Lot of adult dogs here';
+    } else if(oldDogs > adults){
+      return "It's practically the senior center";
+    } else {
+      return "It's difficult to tell..."
+    }
+  }
   const calculateAverageSize = (atTheParkList) => {
     let smallDogCount = 0; let medDogCount = 0; 
     let largeDogCount = 0; let giantDogCount = 0;
@@ -24,16 +51,17 @@ function Park(props){
     return dogSizeString.concat('Small dogs: ' + smallDogCount + ' Medium dogs: ' + medDogCount + ' LargeDogs: ' + largeDogCount + ' GiantDogs: ' + giantDogCount);
   }
   
-  console.log(numberOfDogsAtThePark);
   //Properties to caluclate
   const totalDogSize = calculateAverageSize(atTheParkList);
-  console.log(totalDogSize);
+  const averageDogAge = calculateAverageAge(atTheParkList);
+  console.log(averageDogAge);
   
   return(
     <React.Fragment>
       <div>
         <p>Total checked in dogs: {numberOfDogsAtThePark}</p>
         <p>{totalDogSize}</p>
+        <p>Average Age: {averageDogAge}</p>
       </div>
     </React.Fragment>
   )
