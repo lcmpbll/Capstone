d64bf15758a5239207b87d13158dda01f9b1e146
Liam Cammpbell <liam22campbell@gmail.com>
2023-01-31 22:48:08 -0800

    Add a notfound page

---
+++ src/scenes/NotFound.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NotFound = () => {
+  const notFoundPageStyle = {
+    margin: 'auto',
+    
+    fontColor: 'grey'
+  }
+  return (
+    <>
+      <div style={notFoundPageStyle}>
+        <h1>404 Page not founc</h1>
+      </div>
+    </>
+  )
+}
+
+export default NotFound;
