42f73237dde13e62dc4c151c4f96a570e06f2096
Liam Cammpbell <liam22campbell@gmail.com>
2023-01-31 22:47:59 -0800

    Display dog details, wip display friends.

--- src/onHold/DogDetail.jsx
+++ src/onHold/DogDetail.jsx
@@ -1,70 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { updateDog } from '../functions/apihelper';
+import { useParams } from 'react-router-dom';
+import { updateDog, fetchDog } from '../functions/apihelper';
 
 
 
-const DogDetail = (props) => {
+
+const DogDetail = () => {
+  const [ dog, setDog ] = useState(null);
+  const [ageError, setAgeError] = useState(null);
+  const { id } = useParams();
+  
+  useEffect(() => {
+    fetchDog(id)
+    // .then((res) => res.json())
+    .then((data) => setDog(data));
+  }, [id])
+  
+  console.log(id);
+  if(!dog) return null;
   // const {dog, dogList, onClickingDelete, onClickingFriend, onClickingGo, error} = props;
   // const displayedFriends = dogList.filter(function(dogFriends){
   //   return dog.friendsArray.indexOf(dogFriends.id) !== -1;
   // });
   
-  // const handleEditingDogInList = async (dog) => {
-  //   await updateDog(dog);
+  const handleEditingDogInList = async (dog) => {
+    await updateDog(dog);
     
-  // }
+  }
   
-  // const handleGoingToThePark = (id) => {
-  //   const dogGoingToPark = mainDogList.filter(dog => dog.id === id);
-  //   console.log(dogGoingToPark, 'ln95');
-  //     if(dogGoingToPark[0].dogAgeGroup === 'Young Puppy'){
-  //       setAgeError('Your dog is too young to be vaccinated. Please explore alternative exercise and socialization opportunities');
-  //       return ageError;
-  //     } else {
-  //       if(dogGoingToPark[0].atThePark === true){
-  //         const dogAtPark = {
-  //           ...dogGoingToPark[0],
-  //           atThePark: false
-  //         }
-  //         handleEditingDogInList(dogAtPark);
-  //       } else {
-  //         const dogAtPark = {
-  //           ...dogGoingToPark[0],
-  //           atThePark: true
-  //         }
-  //         handleEditingDogInList(dogAtPark);
-  //       }
-  //     }
-  //   }
+  const onClickingGo = (dog) => {
+    // const dogGoingToPark = mainDogList.filter(dog => dog.id === id);
+    // console.log(dogGoingToPark, 'ln95');
+      if(dog.dogAgeGroup === 'Young Puppy'){
+        setAgeError('Your dog is too young to be vaccinated. Please explore alternative exercise and socialization opportunities');
+        return ageError;
+      } else {
+        if(dog.atThePark === true){
+          const dogLeavesPark = {
+            ...dog,
+            atThePark: false
+          }
+          handleEditingDogInList(dogLeavesPark);
+        } else {
+          const dogEntersPark = {
+            ...dog,
+            atThePark: true
+          }
+          handleEditingDogInList(dogEntersPark);
+        }
+      }
+    }
   
   
-  // const displayLikes = dog.dogLikes.join(" ");
-  // const dsiplayDislikes = dog.dogDislikes.join(" ");
+  
+  
+  const displayLikes = dog.dogLikes.join(" ");
+  const dsiplayDislikes = dog.dogDislikes.join(" ");
   
   
   //Styling
-  // const detailsStyles = {
-  //   display: 'flex',
-  //   flexDirection: 'column',
-  //   position: 'relative',
-  //   justifyContent: 'center',
-  //   fontWeight: 'bold',
-  // }
+  const detailsPageStyle = {
+    border: '2px solid black',
+    display: 'grid',
+    gridTemplateColumns: '1fr 2fr'
+  }
+  const headerStyle = {
+    marginLeft: '25%'
+  }
+  const detailsStyles = {
+    margin: '20px',
+    padding: '20px',
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative',
+    justifyContent: 'center',
+    fontWeight: 'bold',
+    background: 'rgba(219, 219, 219, 0.6)',
+  }
   
-  // let atTheParkStatus = null;
-  // let buttonText = null;
-  // if(dog.atThePark === true && error === null){
-  //   buttonText = "leave the park";
-  //   atTheParkStatus = '';
-  // } else {
-  //   buttonText = "Go to the park";
-  //   atTheParkStatus = 'not'
-  // }
+  let atTheParkStatus = null;
+  let buttonText = null;
+  if(dog.atThePark === true && ageError === null){
+    buttonText = "leave the park";
+    atTheParkStatus = '';
+  } else {
+    buttonText = "Go to the park";
+    atTheParkStatus = 'not'
+  }
   return(
-    <>
-    <h1>Dog Details</h1>
-      {/* <div style={detailsStyles}>
+    <div style={detailsPageStyle}>
+    <h1 style={headerStyle}>Dog Details</h1>
+      <div style={detailsStyles}>
         <h1>{dog.dogName}</h1>
         <h2>Is {atTheParkStatus} at the park.</h2>
         <div>
@@ -86,25 +113,29 @@ const DogDetail = (props) => {
         </div>
         <div className='friends'>
           <h2>Friends</h2>
-          {displayedFriends.map((dog) => dog.dogName)}
+          {/* {displayedFriends.map((dog) => dog.dogName)} */}
           <hr/>
         </div>
         <div className='parks'>
           <h2>Parks</h2>
           {dog.dogParks} &nbsp;
-          {error} <br/> 
-          <button  onClick={() => {onClickingGo(dog.id) }} >{buttonText}</button>
+          {ageError} <br/> 
+          <button  onClick={() => {onClickingGo(dog) }} >{buttonText}</button>
           <hr />
         </div>
         <br/>
-        <div className='buttonControl'>
+        {/* <div className='buttonControl'>
           <button onClick={() => onClickingFriend(dog.id)}>Make some Dog friends</button> &nbsp;
           <button onClick={()=> onClickingDelete(dog)}>Remove Dog</button>
-        </div>
-      </div> */}
-    </>
+        </div> */}
+      </div>
+    </div>
   )
+
+  
 }
+
+
 
 DogDetail.propTypes = {
   dog: PropTypes.object,
