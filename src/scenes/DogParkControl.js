a8e96f45409e4f45e21b73bfc3229b768913fee0
Liam Cammpbell <liam22campbell@gmail.com>
2023-02-04 15:34:28 -0800

    Delete dog park control

--- src/scenes/DogParkControl.js
+++ src/scenes/DogParkControl.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import DogList from '../components/DogList';
+import DogList from './DogList';
 
 
 const DogParkCotrol = () => {
   return (
     <>
-      <DogList/>
+      {/* <DogList/> */}
    
-      <button><Link to={"/addDog"} style={{textDecoration: 'none', color: 'black'}}>Add new Dog</Link></button>
+      
     </>
   )
 }
