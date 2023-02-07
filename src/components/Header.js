e7eefdb80b6acc52fa737cddf5cce316f4c10229
Liam Cammpbell <liam22campbell@gmail.com>
2023-02-06 16:03:44 -0800

    Remove sidebar from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import Sidebar from './navigation';
+
 
 function Header(){
   const headerStyles = {
     textAlign: 'center',
+    
   }
-  const sidebarStyle = {
-    justifySelf: 'start'
-  }
+
   return(
     <>
-      <Sidebar style={sidebarStyle}/>
+      
       <h1 style={headerStyles}>Dog Bark</h1>
     </>
   );
