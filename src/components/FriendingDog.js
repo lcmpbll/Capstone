0f6e13c770b3acc81d36d1a2646b6cd436babf75
Liam Cammpbell <liam22campbell@gmail.com>
2023-02-28 13:30:16 -0800

    Move displaying friends list to friending module?

--- src/components/FriendingDog.js
+++ src/components/FriendingDog.js
@@ -41,7 +41,7 @@ function FriendDogForm(props) {
           <option value='none'>--Select a friend for your dog--</option>
           {(dogList).filter(dogs => dogs.id !== props.dog.id).map((dogs) => <option key={dogs.id} value={dogs.id}>{dogs.dogName}</option>)}
         </select>
-
+        <br/>
         <button type="submit" className='btn btn-default'>Submit</button> 
       </form>
     </>
