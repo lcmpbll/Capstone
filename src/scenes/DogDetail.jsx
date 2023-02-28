861fafcc6ee6a44019e28dae4081296cd3c10305
Liam Cammpbell <liam22campbell@gmail.com>
2023-02-28 13:31:36 -0800

    Display dog friends.

--- src/scenes/DogDetail.jsx
+++ src/scenes/DogDetail.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
-import { updateDog, fetchDog, fetchDogs } from '../functions/apihelper';
+import { fetchDog, fetchDogs } from '../functions/apihelper';
 import { switchParkStatus } from '../functions/parkFunctions';
 import FriendDogForm from '../components/FriendingDog';
+
 
 
 
@@ -39,7 +40,8 @@ const DogDetail = () => {
   if(!dog) return null;
   // const {dog, dogList, onClickingDelete, onClickingFriend, onClickingGo, error} = props;
   const displayedFriends = dogList.filter(function(dogFriends){
-    return dog.friendsArray.indexOf(dogFriends.id) !== -1;
+    let dogsFriends = dog.friendsArray.indexOf(dogFriends.id) !== -1;
+    return dogsFriends;
   });
   
   // const handleEditingDogInList = async (dog) => {
@@ -82,12 +84,19 @@ const DogDetail = () => {
     fontWeight: 'bold',
     background: 'rgba(219, 219, 219, 0.6)',
   }
+  const friendsListStyle = {
+    margin: '4px', 
+    display: 'flex',
+    
+  }
   
   let friendForm = null;
   let atTheParkStatus = null;
   let buttonText = null;
+  const dogPotentialFriendsList = dogList.filter(dogs => !dog.friendsArray.includes(dogs.id));
+ 
   if(friending){
-    friendForm = <FriendDogForm  dog={dog} dogList={dogList} />
+    friendForm = <FriendDogForm  dog={dog} dogList={dogPotentialFriendsList} />
   }
   if(dog.atThePark === true && ageError === null){
     buttonText = "leave the park";
@@ -124,8 +133,14 @@ const DogDetail = () => {
         </div>
         <div className='friends'>
           <h2>Friends</h2>
-          {displayedFriends.map((dog) => dog.dogName)}
-          <button onClick={() => (setFriending(!friending))}>Make Friends</button>
+          <p style={friendsListStyle}>
+            {displayedFriends ? displayedFriends.map((dog) => <FriendedDog
+               dogName={dog.dogName}
+               key={dog.dogId} />
+               ): null}
+          </p>
+          <br/>
+          <button onClick={() => (setFriending(!friending))}>{friending ? `Close Friend Form` : `Make Some Friends`}</button>
           {friendForm}
           <hr/>
         </div>
@@ -143,12 +158,21 @@ const DogDetail = () => {
         </div> */}
       </div>
     </div>
-  )
+  );
 
   
 }
 
-
+const FriendedDog = (props) => {
+  const dogsFriendsStyle = {
+    margin: '4px'
+  }
+  return (
+    <div style={dogsFriendsStyle}>
+      <p>{props.dogName}</p>
+    </div>
+  );
+};
 
 DogDetail.propTypes = {
   dog: PropTypes.object,
