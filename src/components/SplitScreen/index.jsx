53a33d1325ae8486d539562409e836e7e41d7327
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-13 13:36:43 -0700

    wip make split screen

---
+++ src/components/SplitScreen/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from ''
+
+export const SplitScreen = ({
+  childern,
+  leftWeight = 1,
+  rightWeight = 1
+}) => {
+  const [left, right] = childern
+  return (
+    
+  )
+}
