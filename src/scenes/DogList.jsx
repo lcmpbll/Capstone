42687bdf84e3567fff7891083e277d899d238ea5
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-31 14:54:38 -0700

    Add sorting function for dogs.

--- src/scenes/DogList.jsx
+++ src/scenes/DogList.jsx
@@ -18,6 +18,34 @@ const DogList = ({match}) => {
     const newDogList = await fetchDogs();
     setDogList(newDogList);
   }
+   
+  const [sortBy, setSortBy] = useState(null);
+  const [smallest, setSmallest] = useState(true);
+  const [filterBy, setFilterBy] = useState(null);
+  const handleSortClick = (option, smallest) => {
+    if(sortBy === option){
+      setSmallest(!smallest);
+    }
+    setSortBy(option);
+    console.log(sortBy, smallest)
+    sortDogList(sortBy, smallest)
+  }
+  const sortDogList = (sortBy, smallest) => {
+    if(smallest === true){
+      return dogList.sort((a, b) => {
+        return a[sortBy] - b[sortBy]
+      })
+    } else {
+      return dogList.sort((a, b) => {
+        return b[sortBy] - a[sortBy] 
+      })
+    }
+    
+  }
+  
+
+
+  // console.log(dogList)
   
   //styles
   const dogsStyle = {
@@ -36,6 +64,7 @@ const DogList = ({match}) => {
     padding: '13px',
 
   }
+  
   const dogListStyle = {
     margin: '10px, 60px',
     paddingLeft: '150px',
@@ -54,16 +83,40 @@ const DogList = ({match}) => {
     
   }
   
+  const sortButtonStyle = {
+    margin: '5px',
+    width: 'content',
+    border: '2px solid',
+    // borderRadius: '50%',
+    
+  }
+  
   const linkStyle = {
     textDecoration: 'none',
     color: 'black'
   }
+  
   return (
     <div style={dogsStyle}>
       
       <div style={dogListStyle}>
         <h1 style={{marginLeft: '150px'}}>All Dogs</h1>
-        {dogList.map((dog) =>
+        <div>
+          <h4>Sort Dogs:</h4>
+          <div>
+            <button  onClick={() => handleSortClick(`dogAge`, smallest)} style={sortButtonStyle}>Age</button>
+            <button  onClick={() => handleSortClick(`dogWeight`, smallest)} style={sortButtonStyle}>Size</button>
+         
+          </div>
+          {/* <div>
+            <h4>Filter Dogs:</h4>
+            <div>
+              <button  onClick={() => handleFilterClick(`likesPuppies`)} style={sortButtonStyle}>Likes Pupppies</button>
+              <button  onClick={() => handleFilterClick(`female`)} style={sortButtonStyle}>Female</button>
+            </div>
+          </div> */}
+        </div>
+        {dogList.map((dog) => 
         <div style={dogCardStyle} className='card' key={dog.id}>
           <Link to={`dog/${dog.id}`} style={linkStyle}>
             <Dog 
@@ -82,8 +135,10 @@ const DogList = ({match}) => {
         <AtThePark dogList={dogList}/>
       </div>
     </div>
-  );
+  )
 }
+
+
 
 DogList.propTypes = {
   dogList: PropTypes.array,
