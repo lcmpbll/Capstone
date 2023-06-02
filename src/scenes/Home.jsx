0d858631a53af66a0a24772df63aba85d97f8ef7
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-02 10:59:31 -0700

    Wip add swipe sceen for mobile users

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import {DogListWrappedPark, DogListWrappedDogs} from '../WrappedComponents/DogListWrapped';
 import { SplitScreen } from '../components/SplitScreen';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import SwipeScreen from '../components/SwipeScreen';
 
 export const Home = () => {
-
- 
+  const isNonMobile = useMediaQuery('(min-width:600px)');
+  console.log(isNonMobile)
   return (
- 
-      <SplitScreen
-     
-        leftWeight={1}
-        rightWeight={1}
-      >
-        <DogListWrappedDogs/>
-
-        
-        <DogListWrappedPark />
-      </SplitScreen>
-    );
+    <>
+      { isNonMobile ? 
+        <SplitScreen
+          leftWeight={1}
+          rightWeight={1}
+        >
+          <DogListWrappedDogs/>
+          <DogListWrappedPark />
+        </SplitScreen>
+  
+        :
+        <SwipeScreen>
+          <DogListWrappedDogs/>
+          <DogListWrappedPark />
+        </SwipeScreen>
+      }
+    </>
+  );
 }
 
 
