17d46b84dcd96c50aa93b0244dfef6175f5303c6
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-04 18:19:38 -0700

    Add mobile styles for myDogs.

--- src/scenes/MyDogs.jsx
+++ src/scenes/MyDogs.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState, useContext} from 'react';
 import {Link} from 'react-router-dom';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 // import PropTypes from 'prop-types';
+import { useMediaQuery } from '@mui/material';
 import {AppContext} from '../components/App';
 import Dog from '../components/Dog';
 
@@ -11,6 +12,7 @@ const MyDogs = (props) => {
   const {dogList} = props;
   const { currentUser } = useContext(AppContext);
   const [myDogs, setMyDogs] = useState([]);
+  const isNonMobile = useMediaQuery('(min-width:600px)');
   useEffect(() => {
     if(dogList !== null){
       const myDogsList = dogList.filter(dog => dog.ownerId === currentUser.id);
@@ -26,9 +28,9 @@ const MyDogs = (props) => {
   const dogCardStyle = {
     border: 'solid 2px black',
     padding: '10px',
-    width: '400px',
+    width: isNonMobile ? '400px' : '200px',
     margin: '13px',
-    height: '200px', 
+    height: isNonMobile ? '200px' : '100px', 
     background: 'rgba(219, 219, 219)'
   }
   const linkStyle = {
@@ -38,28 +40,28 @@ const MyDogs = (props) => {
   }
   
   return dogList ? (
+  <div>
+    <h1>Your dogs</h1>
     <div>
-    
-      <h1>Your dogs</h1>
-      <div>
-        
-        {myDogs.map((dog) => 
-          <div style={dogCardStyle} className='card' key={dog.id}>
-            <Link to={`/dog/${dog.id}`} style={linkStyle}>
-              <Dog 
-                dogName={dog.dogName}
-                dogSex={dog.dogSex}
-                dogSize={dog.dogSize}
-                dogAgeGroup={dog.dogAgeGroup}
-                id={dog.id}
-                key={dog.id}
-              />
-              
-            </Link>
-          </div>
-          )}
+      {myDogs.map((dog) => 
+        <div style={dogCardStyle} className='card' key={dog.id}>
+          <Link to={`/dog/${dog.id}`} style={linkStyle}>
+            <Dog 
+              isNonMobile={isNonMobile}
+              dogName={dog.dogName}
+              dogSex={dog.dogSex}
+              dogSize={dog.dogSize}
+              dogAgeGroup={dog.dogAgeGroup}
+              id={dog.id}
+              key={dog.id}
+            />
+            
+          </Link>
+        </div>
+        )}
       </div>
     </div>
+  
   ) : null;
 }
 
