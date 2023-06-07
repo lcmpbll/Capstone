f67201bd05b1d53c837b169fd74a7d028849f0ff
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-07 13:03:24 -0700

    Add function to take all myDogs to the park

--- src/scenes/MyDogs.jsx
+++ src/scenes/MyDogs.jsx
@@ -5,6 +5,7 @@ import { useAuthenticator } from '@aws-amplify/ui-react';
 import { useMediaQuery } from '@mui/material';
 import {AppContext} from '../components/App';
 import Dog from '../components/Dog';
+import { switchParkStatus } from '../functions/parkFunctions';
 
 
 
@@ -22,7 +23,10 @@ const MyDogs = (props) => {
 
   // const myDogs = dogList.filter(dog => dog.ownerId === currentUser.id)
   const { route } = useAuthenticator((context) => [context.route]);
-
+  
+  const handleSendingAllDogs = () => {
+    myDogs.map((dog) => switchParkStatus(dog))
+  }
   
 
   const dogCardStyle = {
@@ -40,11 +44,23 @@ const MyDogs = (props) => {
   }
   
   return dogList ? (
-  <div>
-    <h1>Your dogs</h1>
-    <div>
+  <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+    <div style={{margin: '0px auto', display: 'flex'}}>
+      <h1>Your dogs</h1>
+      <button onClick={() => handleSendingAllDogs()} style={{
+        margin: '15px',
+        width: 'content',
+        height: '30px',
+        border: 'solid 2px #4986e8',
+        borderRadius: '8px',
+        shadow: '0',
+        cursor: 'pointer',
+        alignSelf: 'center'
+      }}>All Dogs to the Park</button>
+    </div>
+    <div style={{margin: '0px auto'}}>
       {myDogs.map((dog) => 
-        <div style={dogCardStyle} className='card' key={dog.id}>
+        <div style={dogCardStyle}  key={dog.id}>
           <Link to={`/dog/${dog.id}`} style={linkStyle}>
             <Dog 
               isNonMobile={isNonMobile}
