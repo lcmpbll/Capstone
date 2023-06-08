c87c8a9aebacdf22860d827e8ba6fd541f9bd1e7
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-08 13:00:15 -0700

    Add filter section

--- src/scenes/DogList.jsx
+++ src/scenes/DogList.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Dog from '../components/Dog.jsx';
 import {useMediaQuery} from '@mui/material';
-
+import {AiOutlineArrowDown, AiOutlineArrowUp} from 'react-icons/ai';
+import {RiFilterOffLine, RiFilterLine} from 'react-icons/ri';
 import PropTypes from 'prop-types';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 //shade sort buttons if sort type is selected. 
 const DogList = (props, {match}) => {
+  
   const {dogList} = props;
   const isNonMobile = useMediaQuery('(min-width:600px)');
   const [sortBy, setSortBy] = useState(null);
   const [smallest, setSmallest] = useState(true);
   const [filterBy, setFilterBy] = useState(null);
+  const [include, setInclude] = useState(true);
+  const [filterApplied, setFilterApplied] = useState(false);
+  const [dogView, setDogView] = useState([]);
+  
+  useEffect(() => {
+    setDogView(dogList)
+  }, [dogList]);
+  
   
   const handleSortClick = (option, smallest) => {
     if(sortBy === option){
@@ -21,16 +31,61 @@ const DogList = (props, {match}) => {
   
     sortDogList(sortBy, smallest)
   }
+  
+  const handleMainFilterClick = () => {
+    setFilterApplied(!filterApplied);
+    setSortBy(null);
+    setSmallest(true);
+    setFilterBy(null);
+    setInclude(true);
+    setDogView(dogList);
+  }
   const sortDogList = (sortBy, smallest) => {
     if(smallest === true){
-      return dogList.sort((a, b) => {
+      return dogView.sort((a, b) => {
         return a[sortBy] - b[sortBy]
       })
+     
     } else {
-      return dogList.sort((a, b) => {
+      return dogView.sort((a, b) => {
         return b[sortBy] - a[sortBy] 
-      })
+      });
+
     }
+    
+  }
+  
+  
+  const handleFilterSelectClick = (property, filter, include) => {
+    if(filter === filterBy){
+      setInclude(!include);
+    }else{
+      setFilterBy(filter);
+    }
+    filterDogList(property, filterBy, include);
+  }
+  
+  const filterDogList = (property, filterBy, include) => {
+    const arrayProperties = ['dogLikes', 'dogDislikes'];
+    if(arrayProperties.includes(property) === false){
+      
+      if(include === true){
+        const newDogView = dogList.filter((dog) => dog[property] === filterBy)
+        setDogView(newDogView);
+      } else {
+        
+        const newDogView = dogList.filter((dog) => dog[property] !== filterBy) 
+        setDogView(newDogView);
+      }
+    } else {
+
+      const newDogView = dogList.filter((dog) => dog[property].includes(filterBy) === include);
+      console.log(newDogView);
+      setDogView(newDogView);
+ 
+    }
+    
+    
     
   }
   
@@ -48,7 +103,7 @@ const DogList = (props, {match}) => {
     margin: '13px auto',
     width:  isNonMobile ? '400px' : '200px',
     background: 'rgba(219, 219, 219)',
-    padding: '13px',
+    padding: '1rem',
 
   }
   
@@ -56,7 +111,7 @@ const DogList = (props, {match}) => {
     display: 'flex',
     justifyContent:'center',
     flexDirection: 'column',
-    margin: 'auto',
+    margin: '0 auto',
  
   }
 
@@ -69,6 +124,7 @@ const DogList = (props, {match}) => {
     borderRadius: '8px',
     shadow: '0',
     cursor: 'pointer',
+    alignItems: 'center',
     
   }
   
@@ -79,23 +135,30 @@ const DogList = (props, {match}) => {
 
   return dogList ? (
     <div style={dogsStyle}>
-        <h1 style={{ display: 'flex', justifyContent: isNonMobile ? 'center' : 'flex-start', padding: '1rem'}}>All Dogs</h1>
+      <div style={{display: 'flex', alignItems: 'center', marginBottom: '0'}}>
+        <h1 style={{ display: 'flex', justifyContent: isNonMobile ? 'center' : 'flex-start', padding: '1rem', marginBottom: '0px'}}>All Dogs</h1>
+        <div  onClick={() => handleMainFilterClick()} style={{ border: 'solid 2px  #4986e8', background: filterApplied ? '#4986e8': 'none', width: '2rem', justifyContent: 'center', alignItems: 'center', height: '2rem', padding: '6px', borderRadius: '50%', marginTop: '10px'}} aria-label={filterApplied ? 'Remove all filters and close filter selection.': 'View filter and sort selection.'}>
+          {filterApplied ? <RiFilterOffLine/> : <RiFilterLine/> }
+        </div>
+      </div>
       <div style={dogListStyle}>
+        {filterApplied ? 
         <div>
-          <h4>Sort Dogs:</h4>
+          <h4 sx={{margin: '0px'}}>Sort Dogs:</h4>
           <div>
-            <button  onClick={() => handleSortClick(`dogAge`, smallest)} style={sortButtonStyle}>Age</button>
-            <button  onClick={() => handleSortClick(`dogWeight`, smallest)} style={sortButtonStyle}>Size</button>
+            <button  onClick={() => handleSortClick(`dogAge`, smallest)} style={sortButtonStyle}>{smallest ? <AiOutlineArrowUp/> : <AiOutlineArrowDown/> } Age</button>
+            <button  onClick={() => handleSortClick(`dogWeight`, smallest)} style={sortButtonStyle}>{smallest ? <AiOutlineArrowUp/> : <AiOutlineArrowDown/> } Size</button>
           </div>
-          {/* <div>
+          <div>
             <h4>Filter Dogs:</h4>
             <div>
-              <button  onClick={() => handleFilterClick(`likesPuppies`)} style={sortButtonStyle}>Likes Pupppies</button>
-              <button  onClick={() => handleFilterClick(`female`)} style={sortButtonStyle}>Female</button>
+              <button  onClick={() => handleFilterSelectClick(`dogLikes`, `puppies`, include)} style={sortButtonStyle}>Likes Pupppies</button>
+              <button  onClick={() => handleFilterSelectClick(`dogSex`, `Female`, include)} style={sortButtonStyle}>{include ? "Female" : "Male"}</button>
             </div>
-          </div> */}
+          </div>
         </div>
-        {dogList.map((dog) => 
+        : null }
+        {dogView?.map((dog) => 
         <div style={dogCardStyle} key={dog.id}>
           <Link to={`/dog/${dog.id}`} style={linkStyle}>
             <Dog 
@@ -124,6 +187,12 @@ const DogList = (props, {match}) => {
 DogList.propTypes = {
   dogList: PropTypes.array,
   Dog: PropTypes.object,
+  handleSortClick: PropTypes.func,
+  sortDogList: PropTypes.func,
+  handleMainFilterClick: PropTypes.func,
+  filterDogList: PropTypes.func,
+  handleFilterSelectClick: PropTypes.func,
+  
 };
 
 export default DogList;
