6115a52ba46c27b587d28877c8c9f51b2fd670cc
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-14 12:22:39 -0700

    Clean up

--- src/components/FriendingDog.js
+++ src/components/FriendingDog.js
@@ -8,56 +8,38 @@ function FriendDogForm(props) {
   useEffect(() => {
     
   }, [dog])
+  
   const officialDogList = dogList.filter(dogs => pendingFriendsList.includes(dogs.id) === false);
 
-  // const [target, setTarget] = useState(null);
-  const [friendsList, setFriendsList] = useState([]);
-  const [pendingList, setPendingList] = useState([]);
+
   const [isPending, setIsPending] = useState(false);
   const [target, setTarget] = useState(null);
-  console.log(target, "target");
-  console.log(isPending)
-  //Whenn the value changes setis pending based on if the dog already is in the targetDogs pending friends array
+
+  
+  //When the value changes setispending based on if the dog already is in the targetDogs pending friends array
   const handlePendingCheck = (event) => {
     const {value} = event.target;
     const currentTarget = dogList.find(pup => pup.id === value);
     setTarget(currentTarget);
-    // setTarget(value)
     if(currentTarget.pendingFriends.indexOf(dog.id) !== -1 || currentTarget.friendsArray.indexOf(dog.id) !== -1){
       setIsPending(true)
     } else {
       setIsPending(false);
     }
-    // if the  target dog already has the current dog in the pending array
-    // if(value && value.pendingFriends.indexOf(dog.id) !== -1){
-    //   const newFriendsArray = dog.friendsArray.concat(value.id);
-    //   setFriendsList(newFriendsArray);
-    //   return friendsList;
-      
-    // } else {
-    //   setFriendsList(dog.friendsArray);
-    //   return friendsList;
-    // }
   }
   
   const handleFriendChange = () => {
     if(isPending === true){
       const newFriendsList = dog.friendsArray.concat(target.id);
-      setFriendsList(newFriendsList, 'ufl');
-      console.log(friendsList);
       return newFriendsList;
     } else {
-      const newFriendsList = dog.friendsArray;
-      setFriendsList(newFriendsList);
-      console.log(friendsList, 'fl')
+   
       return dog.friendsArray;
     }
   }
   
   const updateTargetDog = async () => {
-
     if(target !== null){
-      
       const newPendingArray = target.pendingFriends.filter((id) => id !== dog.id);
       const newFriendsArray = target.friendsArray.concat(dog.id);
       const updatedTargetDog = {
@@ -68,15 +50,11 @@ function FriendDogForm(props) {
       await updateDog(updatedTargetDog);
     }
   }
+  
   const handleNewRequest = () => {
-    // const {value} = event.target;
-    // const newTarget = dogList.find(dogs => dogs.id === value.id);
-    console.log(target, 'ln 74')
-    console.log(isPending);
     if(target !== null){
       if(isPending === true){
-        // const newPendingArray = dog.pendingFriends;
-        // setPendingList(dog.pendingFriends);
+      
         return dog.pendingFriends;
       } else {
       
@@ -86,10 +64,9 @@ function FriendDogForm(props) {
       }
     }
   }
+  
   const handleFriendDogFormSubmission = async(event) => {
     event.preventDefault();
-    console.log(isPending);
-    
     const dogWithNewFriend = {
       ...dog,
       friendsArray: handleFriendChange(),
